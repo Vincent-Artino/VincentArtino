fc434c788225c341ed7b7678da2e755e08cf65f7
Vincent-Artino <Vincent-Artino@users.noreply.github.com>
2017-02-27 12:06:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,152 @@
+express = require('express');
+https = require('https');
+request = require('request');
+var app = express();
+port = Number(process.env.PORT || 5000);
+var city,text,temp,temperature;
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+access_token="EAAXo0ZADqGgkBAFRAoQYZB8zZAGr47ci68Q17zBqWKsiiLImBMZAuZBIJd3R7k7pSEBPlHHKOTv4AADnZBQwNZBVQg34ky7hFgbQukq8ZAeGyan9IV5MPDKrYaYC5zopBum5YDSj5ruiWDTSAeCAGYWLVh9XZAoVZC16FJ1qpusiGjFAZDZD";
+
+/*var options = {
+  uri: "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAXo0ZADqGgkBAFRAoQYZB8zZAGr47ci68Q17zBqWKsiiLImBMZAuZBIJd3R7k7pSEBPlHHKOTv4AADnZBQwNZBVQg34ky7hFgbQukq8ZAeGyan9IV5MPDKrYaYC5zopBum5YDSj5ruiWDTSAeCAGYWLVh9XZAoVZC16FJ1qpusiGjFAZDZD",
+  method: 'POST',
+  json: {
+  "setting_type" : "call_to_actions",
+  "thread_state" : "existing_thread",
+  "call_to_actions":[
+    {
+      "type":"postback",
+      "title":"Help",
+      "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
+    }
+  ]
+}
+};
+request.post(options,function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+	console.log("menu set");  
+}
+	else
+	console.log("nope");
+})
+*/
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'Vincent') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+app.post('/webhook', function (req, res) {
+	var data = req.body;
+	if(data.object === 'page'){
+	data.entry.forEach(function(entry){
+		var pageId = entry.id;
+		var time = entry.time;
+		entry.messaging.forEach(function(event){
+		if(event.message){
+			receivedMessage(event);		
+		}
+		else{
+			console.log("Unknown event : ",event);
+		}
+	});
+});
+res.sendStatus(200);
+}
+});
+
+function receivedMessage(event){
+	message = event.message;
+	senderID = event.sender.id;
+	console.log("received message from "+event.sender.id);
+	var messageText = message.text;
+	var messageAttachments = message.attachments;
+	if(messageText){
+		processMessage(senderID,messageText);
+	}
+}
+
+function processMessage(senderID,messageText){
+	if(messageText.includes("tell me about ")){
+			text = messageText.replace("tell me about ","");
+			duck(senderID,text);
+		}
+	return messageText;
+}
+function duck(senderID,text){
+Burl = "http://api.duckduckgo.com/?q="+text+"&format=json&pretty=1";
+	request({
+	url : Burl,
+	json : true 	
+	},function(error,response,body){
+	if(!error){
+		if(body!=null){
+			if(body.Abstract==null){
+			if(body.Definition==null){
+				str = body.Results;
+			}
+			else
+			str = body.Definition;
+			}
+			else
+			str = body.Abstract;
+			sendImage(senderID,body.Image);
+			sendTextMessage(senderID,str);	
+		}	
+	}	
+});
+
+}
+function sendTextMessage(recID,messText){
+	var messageData = {
+	recipient : {
+		id : recID
+	},
+	message: {
+	text:messText	
+	}
+}
+	sendMessage(messageData);
+}
+function sendImage(recId,Iurl){
+	var messageData = {
+	recipient : {
+		id : recId	
+	},
+	message : {
+		attachment : {
+			type : "image",
+			payload : {
+				url : Iurl		
+			} 
+		}
+	}
+}
+	console.log("imaging : "+Iurl);
+	sendMessage(messageData);
+}
+
+function sendMessage(messageData){
+request({
+	uri: 'https://graph.facebook.com/v2.6/me/messages',
+  	qs: { access_token: access_token },
+    	method: 'POST',
+    	json: messageData
+	
+},function (error,response,body){
+	if(!error){
+		console.log("message sent");
+		 	
+	}	
+});
+}
+app.listen(port);
