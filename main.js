6f7e4fd96516704dad3a666cf0c6cf31fa594e3b
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2017-04-07 20:00:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ function score(senderID,id){
 	console.log(id)
 	request({
     	headers: {
-		"unique_id": id,
+		"unique_id": 1082593,
 		"apikey": "RqykOVNrgVUMeZye189OQ3SaB7k1 "
     	},
    	uri: "http://cricapi.com/api/cricketScore",
