b8c9bcf37c5a2e6ccab1515e94bdc8baff3a8295
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2017-04-07 15:18:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -35,9 +35,8 @@ app.post('/webhook', function (req, res) {
 			console.log("Unknown event : ",event);
 			if(event.postback){
 				if(event.postback.payload.includes("play video ")){
-					text = event.postback.payload
-					console.log(text + text.replace("play video ",""))
-					sendVideo(event.sender.id,"https://youtu.be/"+text.replace("play video "))
+				
+					sendVideo(event.sender.id,"https://youtu.be/"+event.postback.payload.replace("play video ",""))
 				}
 			}
 		}
