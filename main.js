4e1d9fb35c3e0ffe965efe0d42194696377a625c
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2017-04-24 18:09:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ function receivedMessage(event){
 		else
 		processMessage(senderID,messageText.toLowerCase());
 	}
-	else if(messageAttachments){
+	else if(messageAttachments.type=='location'){
 			console.log(messageAttachments)
 			if(location[senderID.toString()]['lat']=='NA'&&location[senderID.toString()]['lon']=='NA'){
 				var loc = []
