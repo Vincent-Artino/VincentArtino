f5af59cdbbef60213e7bf7fc18fe941d846bb5e5
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2017-04-24 20:26:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -195,7 +195,7 @@ function processMessage(senderID,messageText){
 	}
 }
 function places(senderID,text){
-	var query = text.replace("#pl ")
+	var query = text.replace("#pl ","")
 	console.log("in places "+location)
 	var coord = location[senderID.toString()]['lat']+","+location[senderID.toString()]['lon']
 	var imgBaseURL = "https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyCsojMsfWiHhc4RwlXmfGBbNy747m5oAk8&photoreference="
